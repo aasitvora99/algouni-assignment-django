e470539a10e7c694b4e1c0605a352b50176e69f1
Aasit Vora <aasitvora1999@gmail.com>
2024-02-18 11:35:19 +0530

    MAINT: maintainance commits

--- algobackend/ide/views.py
+++ algobackend/ide/views.py
@@ -1,7 +1,7 @@
 import subprocess
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from .models import CodeSubmission, CodeOutput # Assuming you have the serializers defined
+from .models import CodeSubmission, CodeOutput
 from .serializers import CodeSubmissionSerializer, CodeOutputSerializer
 
 class ExecuteCodeView(APIView):
@@ -9,19 +9,12 @@ class ExecuteCodeView(APIView):
         code = request.data.get('code')
         language = request.data.get('language')
         test_case_input = request.data.get('input')
-
-        # ... Logic to execute code based on 'language' ...
-        #     (This is where things get language-specific) 
-
-        # Assuming Python execution for simplicity:
         try:
             output = subprocess.check_output(['python', '-c', code], input=test_case_input, encoding='utf-8')
-            verdict = 'Pass'  # Adjust if the output needs verification
+            verdict = 'Pass' 
         except subprocess.CalledProcessError as e:
             output = e.output.decode('utf-8')
             verdict = 'Runtime Error' 
-
-        # Store CodeSubmission and CodeOutput in the database ... 
 
         return Response({
             'output': output,
@@ -30,9 +23,7 @@ class ExecuteCodeView(APIView):
 
 class UserSubmissionsView(APIView):
     def get(self, request):
-        user = request.user  # Assuming authentication 
+        user = request.user  # auth 
         submissions = CodeSubmission.objects.filter(user=user)
         submissions_data = CodeSubmissionSerializer(submissions, many=True).data
-
-        # Assuming a nested relationship for output in your serializer
         return Response(submissions_data) 
