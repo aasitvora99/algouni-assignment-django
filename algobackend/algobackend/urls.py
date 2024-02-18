8b43d9e037feb2e1c9c926d37e90effef8b33714
Aasit Vora <aasitvora1999@gmail.com>
2024-02-18 13:37:16 +0530

    DEV: added additional routes for api calls

--- algobackend/algobackend/urls.py
+++ algobackend/algobackend/urls.py
@@ -1,15 +1,17 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
-from ide.views import ExecuteCodeView
-from ide.views import UserSubmissionsView
+from ide.views import *
 
 router = routers.DefaultRouter()
 # router.register(r'problems', ProblemViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/problems/create/', CreateProblemView.as_view()), 
     path('api/', include(router.urls)), 
     path('api/execute/', ExecuteCodeView.as_view()),
-    path('api/submissions/', UserSubmissionsView.as_view())
+    path('api/submissions/', UserSubmissionsView.as_view()),
+    path('api/problems/', ProblemListView.as_view()), 
+    path('api/problems/<int:pk>/', ProblemDetailView.as_view()), 
 ]
